40c52c4f2f1b7f1aade52310dce9da102397e89a
Jacky So <so.jacky311.dev@gmail.com>
2014-04-25 23:02:41 -0400

    Add initial glassware project to git

---
+++ Glassware/gen/com/gghackthonv2/toronto_311_glassware/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.gghackthonv2.toronto_311_glassware;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}

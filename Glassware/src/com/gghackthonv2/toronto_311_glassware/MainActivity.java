6c0a74bf0bb0a832f3feb553bf3d3770b9bc7503
Jacky So <so.jacky311.dev@gmail.com>
2014-04-26 17:30:00 -0400

    Create top layer structure ActionView

--- Glassware/src/com/gghackthonv2/toronto_311_glassware/MainActivity.java
+++ Glassware/src/com/gghackthonv2/toronto_311_glassware/MainActivity.java
@@ -1,61 +1,111 @@
 package com.gghackthonv2.toronto_311_glassware;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.gghackthonv2.view.MainActionView;
+import com.gghackthonv2.view.MainActionView.MainActionType;
+import com.google.android.glass.media.Sounds;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
+
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.AdapterView;
 
 public class MainActivity extends Activity {
+
+	// Predefine MainAction Object
+	private final List<MainActionType> mainActions = Arrays.asList(MainActionType.SIDEWALK, MainActionType.ROAD,
+			MainActionType.CITY_PROPERTY, MainActionType.NOISE, MainActionType.CONTACT, MainActionType.MY_ISSUES);
+
+	private class CardScrollViewAdapter extends CardScrollAdapter {
+
+		@Override
+		public int getCount() {
+			return mActionViews.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return mActionViews.get(position);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			return mActionViews.get(position);
+		}
+
+		@Override
+		public int getPosition(Object item) {
+			return mActionViews.indexOf(item);
+		}
+	}
+
+	private List<MainActionView> mActionViews;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		createActionViews();
+
+		CardScrollViewAdapter adapter = new CardScrollViewAdapter();
+
+		CardScrollView cardScrollView = new CardScrollView(this);
+		cardScrollView.setHorizontalScrollBarEnabled(true);
+		cardScrollView.setAdapter(adapter);
+		cardScrollView.activate();
+		cardScrollView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				MainActionType type = mainActions.get(position);
+				selectedOption(type);
+			}
+		});
+
+		// set the view of this activity
+		setContentView(cardScrollView);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
+	private void createActionViews() {
+		mActionViews = new ArrayList<MainActionView>();
+		for (MainActionType mainAction : mainActions) {
+			MainActionView mainActionView = new MainActionView(this, mainAction);
+			mActionViews.add(mainActionView);
 		}
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-			return rootView;
-		}
 	}
 
+	private void selectedOption(MainActionType type) {
+		Intent intent;
+
+		switch (type) {
+			case SIDEWALK:
+	
+			case ROAD:
+	
+			case CITY_PROPERTY:
+	
+				break;
+			case NOISE:
+				
+				break;
+			case CONTACT:
+	
+				break;
+			case MY_ISSUES:
+	
+				break;
+			default:
+				// Something is wrong
+				return;
+		}
+	}
 }

a2bdf428a86be2741955b2e1ee124f25275d9f23
Jacky So <so.jacky311.dev@gmail.com>
2014-04-26 15:34:50 -0400

    update .gitignore

--- Glassware/gen/com/gghackthonv2/toronto_311_glassware/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.gghackthonv2.toronto_311_glassware;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}

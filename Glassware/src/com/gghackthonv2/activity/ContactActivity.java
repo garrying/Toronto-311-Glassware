5fb5ff011c43a4cb03f20e917401933e999776e9
Stefan Panait <stefanapanait@gmail.com>
2014-04-27 11:51:16 -0400

    contact activity and layout created

---
+++ Glassware/src/com/gghackthonv2/activity/ContactActivity.java
@@ -0,0 +1,41 @@
+package com.gghackthonv2.activity;
+
+import com.gghackthonv2.toronto_311_glassware.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ContactActivity extends Activity {
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Intent intent = getIntent();
+		// Get intent extras for the img and description
+		// intent.getParcelableExtra(MainActivity.EXTRA_SELECT_ACTION_TYPE);
+		
+//		311 Toronto
+//
+//		Phone within Toronto city limits: 311
+//		Phone outside city limits: 416-392-CITY (2489)
+//		(Can be used within Toronto if you can't reach 311.)
+//
+//		TTY customers: 416-338-0TTY (0889)
+//		Fax: 416-338-0685
+//		E-mail: 311@toronto.ca
+//		(For urgent matters, please call us. We are open 24/7.)
+//
+//		Self-service: toronto.ca/311
+//
+//		Always call 911 for emergencies.
+//
+//		Connect with us on Twitter @311toronto
+		
+	}
+	
+
+}
